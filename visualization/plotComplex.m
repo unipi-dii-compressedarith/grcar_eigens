cacdbb9eeaa665b746af00233ce42c05ade25d71
Federico Rossi <f.rossi57@studenti.unipi.it>
2023-07-29 16:59:22 +0200

    Multiple plots

--- visualization/plotComplex.m
+++ visualization/plotComplex.m
@@ -1,7 +1,11 @@
-[eigs,eigsT] = readComplexFile("out_p64.txt");
+[eigs_d,eigsT_d] = readComplexFile("out100_double.txt");
+[eigs_p32,eigsT_p32] = readComplexFile("out100_p32.txt");
+[eigs_p64,eigsT_p64] = readComplexFile("out100_p64.txt");
 
-plot(eigs,'o');
+plot(eigs_d,'o');
 hold on
-plot(eigs,'*');
-legend("Original","Transposed");
-title("GRCAR Size="+(size(eigs,1)));
+plot(eigsT_d,'*');
+plot(eigs_p32,'+');
+plot(eigsT_p32,'square');
+legend("Double","DoubleT","P32","P32T");
+title("GRCAR Size="+(size(eigs_d,1)));
